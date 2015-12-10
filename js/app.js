281b36dd992bf5d76abea8ed57a99277284d85d2
Silverhawk <sondra.silverhawk@hush.com>
2015-12-10 11:51:02 -0800

    Update index with correct links

--- js/app.js
+++ js/app.js
@@ -9,7 +9,14 @@
 		{
 			date : '12/09/15',
 			title : 'Hello, World!',
-			description : 'An introductory page, to say hi to the world. Hey, world!'
+			description : 'An introductory page, to say hi to the world. Hey, world!',
+			hash : '120915.html'
+		},
+		{
+			date : '12/10/15',
+			title : 'Advent Calendar',
+			description : 'A simple countdown until Christmas, created with HTML, CSS, and jQuery.',
+			hash : '121015.html'
 		}
 	];
 })();
