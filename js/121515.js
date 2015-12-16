e57181828dcf271f96ce157c16e448415bff0bf9
Silverhawk <sondra.silverhawk@hush.com>
2015-12-15 17:53:15 -0800

    Add files for word soup
    ...including a resources dir with text files

---
+++ js/121515.js
@@ -0,0 +1,31 @@
+$(function() {
+  var pathway = "../resources/"
+  var texts = ['descartes', 'bacon', 'aristotle', 'plato', 'russell', 'camus', 'hobbes', 'kafka', 'hume', 'kant', 'locke', 'tzu', 'nietzsche'];
+  var getRandomText = function(arr) {
+    return pathway + arr[Math.floor(Math.random() * arr.length)] + '.txt';
+  }
+  var getRandomSentence = function(arr) {
+    var rand = Math.floor(Math.random() * arr.length)
+    return arr[rand] + '. ';
+  }
+
+  var numSentences = 4;
+  
+  $('.generate').on('click', function() {
+    $('.output').html('').show();
+    for(var i = 0; i < numSentences; i++) {
+      var txt = getRandomText(texts);
+      $.ajax({
+        url: txt,
+        dataType: "text",
+        success: function(data) {
+          var txt_array = data.split(".");
+          txt_array.pop();
+          var sentence = getRandomSentence(txt_array);
+          $('.output').append('<p>' + sentence + '</p>');
+        }
+      });
+    }
+  });
+    
+});
